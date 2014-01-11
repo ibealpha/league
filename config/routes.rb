90b9334418c07ee5b17d3658527d8dc042ed9aaa
Evan Berquist <evanberquist@gmail.com>
2014-01-10 23:16:02 -0500

    Finish sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 League::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
-  get "static_pages/home"
+  match '/signup', to: 'users#new',	via: 'get'
+  match '/signin', to: 'sessions#new',	via: 'get'
+  match '/signout', to: 'sessions#destroy',	via: 'delete'
+
+  #get "static_pages/home"
 
 end
