e91b8ce2521f85d927133054cad4b1b9c28ab4b7
Evan Berquist <evanberquist@gmail.com>
2014-02-05 12:14:01 -0500

    another commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :user_teams
 	validates :name,  presence: true, length: { maximum: 25 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 League::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :players
+  resources :user_teams
+  resources :user_players
 
   root to: 'static_pages#home'
 
